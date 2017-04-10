cda5cc69aa6ba02ee2fb2a1657520612609e949e
Ulrich <backes@solthor.de>
2017-04-10 16:09:53 +0200

    1

---
+++ CSharpTour/Hello.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpTour
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
+            Console.WriteLine("Hello world!!!");
+
+            Console.CursorVisible = false;
+            Console.Title = "My Program";
+
+            Console.Beep(1000, 500);
+            Console.Beep();
+
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR :)");
+
+            Console.ReadKey();
+
+            Console.ResetColor();
+            Console.Clear();
+
+        }
+    }
+}
