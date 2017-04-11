c149f2a8fe6609d9164a9aff9a0531079e95aef6
Ulrich <backes@solthor.de>
2017-04-11 10:44:47 +0200

    WebTools added

---
+++ WebRequest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+
+namespace WebRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HttpWebRequest request = HttpWebRequest.CreateHttp("http://www.spiegel.de");
+
+                WebResponse response = request.GetResponse();
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+                Stream dataStream = response.GetResponseStream();   // get bytes
+                StreamReader reader = new StreamReader(dataStream); // get char
+                string responseFromServer = reader.ReadToEnd();
+
+                // Clean up the streams.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+
+                Console.WriteLine(responseFromServer);
+                Console.WriteLine();
+                Console.WriteLine(responseFromServer.Contains("tigermuecke"));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ WebTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+        
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.Beep();
+
+                do
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Bitte geben Sie eine WWW-Adresse an:");
+                    string uriString = Console.ReadLine();
+                    string host;
+                    try
+                    {
+                        UriBuilder uriBuilder = new UriBuilder("http", uriString);
+                        Uri uri = uriBuilder.Uri;
+                        host = uri.Host;
+                        Console.WriteLine();
+                        Console.WriteLine($"Der Host lautet '{host}'");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    Console.WriteLine("Q für beenden. Ansonsten andere Taste");
+                } while (Console.ReadKey(true).Key != ConsoleKey.Q);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
